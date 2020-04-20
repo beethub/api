791f7d57de06533a5878475645778592f7cb9a6d
Cesar Varela <cesarac66@hotmail.com>
2020-04-19 20:08:19 -0500

    Modify Dockerfile and add steps to build typescript

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
-FROM node:9-slim
-ENV PORT 8080
-EXPOSE 8080
+FROM node:10-slim
+ENV PORT 4000
+EXPOSE 4000
+#RUN groupadd -r nodejs && useradd -m -r -g -s /bin/bash nodejs nodejs
+#USER nodejs
+#WORKDIR /home/nodejs/app
 WORKDIR /usr/src/app
-COPY . .
-CMD ["npm", "start"]
+ENV  NODE_ENV production
+COPY package-lock.json package.json ./
+RUN npm install --production
+COPY ./dist .
+CMD ["node", "main.js"]
--- src/schema.ts
+++ src/schema.ts
@@ -2,9 +2,9 @@ import { importSchema } from 'graphql-import';
 import { makeExecutableSchema } from 'graphql-tools';
 import resolvers  from "./resolvers";
 import { KeycloakSchemaDirectives } from "keycloak-connect-graphql";
+import path from "path";
 
-const typeDefs: string = importSchema("src/schema/schema.gql");
-
+const typeDefs: string = importSchema(path.join(__dirname, "./schema/schema.gql"));
 
 const schema = makeExecutableSchema({
   typeDefs: [typeDefs],
